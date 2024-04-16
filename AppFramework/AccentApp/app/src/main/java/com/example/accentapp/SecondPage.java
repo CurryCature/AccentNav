dfa35e1f9db3567e71487d3d434de387928dfdb7
hosnaml <hosna@kth.se>
2024-04-16 14:17:23 +0200

    texts on the second page

--- AppFramework/AccentApp/app/src/main/java/com/example/accentapp/SecondPage.java
+++ AppFramework/AccentApp/app/src/main/java/com/example/accentapp/SecondPage.java
@@ -1,4 +1,5 @@
 package com.example.accentapp;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -6,14 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+public class SecondPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
