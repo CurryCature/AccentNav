664ac09619c07c8d90d84e896dcb1d90f60611ed
Liyagiha <m.mtaseenm@gmail.com>
2024-04-17 14:07:49 +0200

    Thirdpage and loading page demo

---
+++ AppFramework/AccentApp/app/src/main/java/com/example/accentapp/ThirdPage.java
@@ -0,0 +1,36 @@
+package com.example.accentapp;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Timer;
+
+public class ThirdPage extends AppCompatActivity{
+    TextView textView;
+    ImageView logo;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_page);
+
+        textView = (TextView) findViewById(R.id.textView2);
+        logo = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ThirdPage.this, Loading.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
