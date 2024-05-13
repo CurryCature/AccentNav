5c4435672da547fbfe5b06b8783e7121d440c936
Turja <dasturja4@gmail.com>
2024-05-13 13:50:40 +0200

    All Files

---
+++ AccentNav-iOS/Acucent iOS/Acucent_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acucent_iOSApp.swift
+//  Acucent iOS
+//
+//  Created by Turja Das on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct Acucent_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccentNav-iOS/Pods/Target Support Files/Pods-Acucent iOS/Pods-Acucent iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Acucent_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_Acucent_iOS
+@end
